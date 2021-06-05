4bcefdfbd40c11ac28c76c29f14bb050c5658555
Jakub Baliński <balinski.jakub@gmail.com>
2021-06-05 15:19:23 +0200

    Update examples

--- examples/plot_classifier.py
+++ examples/plot_classifier.py
@@ -11,15 +11,15 @@ from sklearn.datasets import make_blobs
 
 from fuzzytree import FuzzyDecisionTreeClassifier
 
-X, y = make_blobs(n_samples=300, n_features=2, centers=[[-23, -12], [12, 42], [52, 2], [-18, 41]],
-                  cluster_std=[10, 25, 12, 11], random_state=42)
+X, y = make_blobs(n_samples=300, n_features=2,
+                  centers=[[10, 0], [20, 30], [40, 5]],
+                  cluster_std=[7, 11, 13], random_state=42)
 
 clf = FuzzyDecisionTreeClassifier().fit(X, y)
 
-ax = plt.plot()
-fig = plot_decision_regions(X=X, y=y, clf=clf, legend=2)
+plot_decision_regions(X=X, y=y, clf=clf, legend=2)
 plt.title("Fuzzy Tree Classification on blobs")
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
+plt.xlabel("Feature 1")
+plt.ylabel("Feature 2")
 
 plt.show()
